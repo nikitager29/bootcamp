5bbd1c369bd210e511b18907162974edf8ef988d
nikitager29 <100104617+nikitager29@users.noreply.github.com>
2022-04-28 03:52:44 +0300

    password validator

---
+++ password-validator.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#echo "enter the password"
+#read passwordd
+password=$1
+len="${#password}"
+int=1
+
+echo -e "\e[1;31m"
+if test $len -ge 10 ; then
+
+    echo "$password" | grep -q [0-9]
+
+     if test $? -eq 0 ; then
+           echo "$password" | grep -q [A-Z]
+
+                if test $? -eq 0 ; then
+
+                    echo "$password" | grep -q [a-z]
+                       if test $? -eq 0 ; then 
+                            echo -e "\e[1;32m Strong password"                              
+                            int=0
+                       else
+
+                         echo "weak password include lower case char"
+
+                       fi
+                   else
+
+                     echo "weak password include capital char" 
+                 fi
+          else
+
+       echo "please include the numbers in password it is weak password"   
+     fi
+
+else
+
+    echo "password length should be greater than or equal 10 hence weak password"
+fi
+echo "exit code = $int"
+read
+exit int
